992586f2680b5eef47c57a59d213a3fdac16ec18
kimdh-hi <zbeld123@gmail.com>
2022-02-02 19:25:12 +0900

    flyway 설정 및 V1 테스트

--- src/main/scripts/mysqlusers.sql
+++ src/main/scripts/mysqlusers.sql
@@ -1,18 +1,18 @@
-# DROP DATABASE IF EXISTS bookdb;
-# DROP USER IF EXISTS `bookadmin`@`%`;
-# DROP USER IF EXISTS `bookuser`@`%`;
-#
-# create database if not exists bookdb character set utf8mb4 collate utf8mb4_unicode_ci;
-#
-# create user if not exists `bookadmin`@`%` identified with mysql_native_password by 'password';
-# grant select, insert, update, delete, create, drop, references, index, alter, execute, create view, show view,
-#     create routine, alter routine, event, trigger on `bookdb`.* to `bookadmin`@`%`;
-#
-# create user if not exists `bookuser`@`%` identified with mysql_native_password by 'password';
-# grant select, insert, update, delete, show view on `bookdb`.* to `bookuser`@`%`;
-#
-# flush privileges;
-#
-#
-#
-#
+DROP DATABASE IF EXISTS bookdb;
+DROP USER IF EXISTS `bookadmin`@`%`;
+DROP USER IF EXISTS `bookuser`@`%`;
+
+create database if not exists bookdb character set utf8mb4 collate utf8mb4_unicode_ci;
+
+create user if not exists `bookadmin`@`%` identified with mysql_native_password by 'password';
+grant select, insert, update, delete, create, drop, references, index, alter, execute, create view, show view,
+    create routine, alter routine, event, trigger on `bookdb`.* to `bookadmin`@`%`;
+
+create user if not exists `bookuser`@`%` identified with mysql_native_password by 'password';
+grant select, insert, update, delete, show view on `bookdb`.* to `bookuser`@`%`;
+
+flush privileges;
+
+
+
+
--- src/test/java/kdh/gurujpa/repository/BookRepositoryTest.java
+++ src/test/java/kdh/gurujpa/repository/BookRepositoryTest.java
@@ -1,16 +1,17 @@
 package kdh.gurujpa.repository;
 
+import kdh.gurujpa.bootstrap.DataInitializer;
 import kdh.gurujpa.domain.Book;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Import;
 
-@ComponentScan(basePackages = {"kdh.gurujpa.bootstrap"})
-@DataJpaTest
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@Import(DataInitializer.class)
+@DataJpaTest
 public class BookRepositoryTest {
 
     @Autowired BookRepository bookRepository;
