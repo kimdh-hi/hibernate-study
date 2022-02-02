82f736b157ef2439e272ccc2f4b07fbcad005848
kimdh-hi <zbeld123@gmail.com>
2022-02-02 13:38:20 +0900

    h2를 mysql mode로 설정, schema.sql 로 DDL 구성 (ddl-auto=validate)

--- src/test/java/kdh/gurujpa/repository/BookRepositoryTest.java
+++ src/test/java/kdh/gurujpa/repository/BookRepositoryTest.java
@@ -1,17 +1,16 @@
 package kdh.gurujpa.repository;
 
-import kdh.gurujpa.bootstrap.DataInitializer;
 import kdh.gurujpa.domain.Book;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Import;
 
 @ComponentScan(basePackages = {"kdh.gurujpa.bootstrap"})
-//@Import(DataInitializer.class)
 @DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 public class BookRepositoryTest {
 
     @Autowired BookRepository bookRepository;
